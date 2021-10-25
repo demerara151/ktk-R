3464ca2bfddf675b8439a36ea4b85b380fce60ff
demerara151 <demeraraonfire@gmail.com>
2021-10-25 10:57:48 +0900

    add batch files

---
+++ auto_change.ps1
@@ -0,0 +1,1 @@
+sed -i 's/0/1/' "D:\SteamGames\steamapps\common\Kritika\resource\Common\database\FontConfig_ja-JP.txt"
---
+++ config/open_fontconfig.bat
@@ -0,0 +1,2 @@
+notepad "C:\Program Files (x86)\Steam\steamapps\common\Kritika\resource\Common\database\FontConfig_ja-JP.txt"
+
