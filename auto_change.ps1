75c71fdb96cb36236222ca51e573ef5d4d2f2514
demerara151 <demeraraonfire@gmail.com>
2021-11-07 02:32:59 +0900

    update auto_change.ps1

--- auto_change.ps1
+++ auto_change.ps1
@@ -1,1 +1,1 @@
-sd -s 'Normal 1.1' 'Normal 1.0' "D:\SteamGames\steamapps\common\Kritika\resource\Common\database\FontConfig_ja-JP.txt"
+sd -s 'Normal 1.0' 'Normal 1.1' "D:\SteamGames\steamapps\common\Kritika\resource\Common\database\FontConfig_ja-JP.txt"
